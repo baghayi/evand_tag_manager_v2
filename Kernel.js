0194294ada2c651c03066e699525053f69123297
Hossein Baghayi <hossein.baghayi@gmail.com>
2019-02-04 16:21:09 +0330

    for fethcing url and other shared functionalities

---
+++ Kernel.js
@@ -0,0 +1,45 @@
+
+class Kernel
+{
+    retrieveJWT(then) {
+        /*
+         *chrome.storage.sync.get(['status'], function(result){
+         *    then(result.status.jwt);
+         *});
+         */
+        then(null);
+    }
+
+    makePageByTabUrl(tabUrl) {
+        let pieces = tabUrl.pathname.split("/");
+
+        switch(pieces[1]) {
+            case 'events': return { type: 'event', slug: pieces[2] };
+            case 'organizations': return { type: 'organization', slug: pieces[2] };
+        }
+    }
+
+    retrieveTabUrl(jwt, then) {
+        const that = this;
+        chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+            let tabUrl = document.createElement('a');
+            tabUrl.href = tabs[0].url;
+
+            const page = that.makePageByTabUrl(tabUrl); 
+            then(jwt, page, tabs[0].id);
+        });
+    }
+
+    run(callWhenReady) {
+        this.callWhenReady = callWhenReady;
+        this.retrieveJWT( (jwt) => this.retrieveTabUrl(jwt, (jwt, tabUrl, tabId) => callWhenReady(jwt, tabUrl, tabId)));
+    }
+    
+    getPopup(page) {
+        switch(page.type) {
+            case 'event': return "Action/EventTags.html";
+            case 'organization': return "Action/OrganizationTags.html";
+        }
+    }
+}
+
